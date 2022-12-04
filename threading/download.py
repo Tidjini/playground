7cf44c09e975d039b3504d70b99b9103b8163c21
Tidjini <tidjini.messaoudi@gmail.com>
2022-12-04 06:07:32 +0100

    set download with thread pool executor

--- threading/download.py
+++ threading/download.py
@@ -1,4 +1,5 @@
 import time
+from concurrent.futures import ThreadPoolExecutor
 import requests
 
 start = time.perf_counter()
@@ -9,12 +10,23 @@ urls = [
     "https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
 ]
 
-for index, url in enumerate(urls):
+
+def download(index, url):
     img_bytes = requests.get(url).content
     img_name = f"image_{index + 1}.jpg"
     with open(img_name, "wb") as img_file:
         img_file.write(img_bytes)
         print(f"{img_name} was downoalded")
+
+
+with ThreadPoolExecutor() as executor:
+    results = executor.map(download, range(4), urls)
+    # for result in results:
+    #     print("re:ends")
+
+# first implementation
+# for index, url in enumerate(urls):
+#     download(url, index)
 
 end = time.perf_counter()
 
